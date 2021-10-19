f6fed665ef9497eeb23e3fce49b347da5dd2b38b
aqmalio <aqmal2399@gmail.com>
2021-10-19 19:24:46 +0700

    init todo-app

---
+++ src/Components/TodoList.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { TodoStack } from './TodoStack'
+
+export const TodoList = () => {
+
+  const todos = [{
+    "id": 1,
+    "title": "Make a meal",
+    "description": "lorem ipsum",
+    "status": 0,
+    "createdAt": "2019-11-15 18:00"
+  }, {
+    "id": 2,
+    "title": "Dinner with family",
+    "description": "lorem ipsum",
+    "status": 0,
+    "createdAt": "2019-11-14 18:00"
+  }, {
+    "id": 3,
+    "title": "Watch scary movie",
+    "description": "lorem ipsum",
+    "status": 0,
+    "createdAt": "2019-11-13 13:00"
+  }, {
+    "id": 4,
+    "title": "Learn something new",
+    "description": "lorem ipsum",
+    "status": 1,
+    "createdAt": "2019-11-12 08:00"
+  }, {
+    "id": 5,
+    "title": "Make a phone call to mom",
+    "description": "lorem ipsum",
+    "status": 1,
+    "createdAt": "2019-11-11 04:00"
+  }]
+
+  return (
+    <div className="w-full flex space-x-4">
+      {/* Todo status 0 (belum selesai)*/}
+      <ul role="list" className="shadow-lg bg-red-100 rounded p-2 w-1/2 divide-y divide-gray-200">
+        {todos.filter((todo) => todo.status == 0).sort((a, b) => (a.createdAt > b.createdAt) ? 1 : -1).map((todo) => (
+          <TodoStack todo={todo}/>
+        ))}  
+      </ul>
+      {/* Todo status 1 (selesai)*/}
+      <ul role="list" className="shadow-lg bg-green-100 rounded p-2 w-1/2 divide-y divide-gray-200">
+        {todos.filter((todo) => todo.status == 1).sort((a, b) => (a.createdAt < b.createdAt) ? 1 : -1).map((todo) => (
+          <TodoStack todo={todo}/>
+        ))}  
+      </ul>
+    </div>
+  )
+}
---
+++ src/Components/TodoStack.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export const TodoStack = (props) => {
+  const todo = props.todo
+  return (
+    <li class="relative bg-white py-5 px-4 hover:bg-gray-50 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600">
+      <div class="flex justify-between space-x-3">
+        <div class="min-w-0 flex-1">
+          <a href="#" class="block focus:outline-none">
+            <span class="absolute inset-0" aria-hidden="true"></span>
+            <p class="text-sm font-medium text-gray-900 truncate">{ todo.title }</p>
+          </a>
+        </div>
+        <time datetime="2021-01-27T16:35" class="flex-shrink-0 whitespace-nowrap text-sm text-gray-500">{ todo.createdAt }</time>
+      </div>
+      <div class="mt-1">
+        <p class="line-clamp-2 text-sm text-gray-600">
+          {todo.description}
+        </p>
+      </div>
+    </li>
+  )
+}
